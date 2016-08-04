d836f949c7711251e78490fd99c77cdd54b1eeb1
Adarsh Nair <adarshnair@Adarshs-MacBook-Pro.local>
2016-08-04 07:59:14 -0700

    ch1_initial

---
+++ Ch1/code.py
@@ -0,0 +1,16 @@
+# Check if string has all unique characters
+# def no_duplicates(stri):
+#     """ Determine if str_ has all unique characters """
+#     print len(stri) == len(set(stri))
+
+# no_duplicates('helo')
+# no_duplicates('hello')
+
+# Reverse a string
+def reverse_string(stri):
+	stri = list(stri)
+	stri = stri[::-1]
+	print ''.join(stri)
+
+reverse_string('adarsh')
+
