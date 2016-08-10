c4ec6bf7a1624703dc5a71a2d1ee124faeb6582b
Adarsh Nair <adarsh108@gmail.com>
2016-08-10 14:53:29 -0700

    ch4

--- Ch4/binarytree.py
+++ Ch4/binarytree.py
@@ -4,6 +4,7 @@ class BinaryTree():
         self.left = None
         self.right = None
         self.parent = None
+        self.depth = -1
 
     def set_left(self,node):
         self.left = node
@@ -13,21 +14,40 @@ class BinaryTree():
         self.right = node
         self.right.parent = self
 
+    
+    def preorder(self):
+        left_vals = self.left.preorder() if self.left is not None else []
+        right_vals = self.right.preorder() if self.right is not None else []
+        return [self.value] + left_vals + right_vals
+
     def inorder(self):
         left_vals = self.left.inorder() if self.left is not None else []
         right_vals = self.right.inorder() if self.right is not None else []
         return left_vals + [self.value] + right_vals
 
+    def postorder(self):
+        left_vals = self.left.preorder() if self.left is not None else []
+        right_vals = self.right.preorder() if self.right is not None else []
+        return left_vals + right_vals + [self.value]
+
 
 if __name__ == '__main__':
+    # Root
     tree = BinaryTree(4)
+    
     left = BinaryTree(3)
     left.set_left(BinaryTree(1))
     left.set_right(BinaryTree(20))
+    
     right = BinaryTree(7)
     right.set_left(BinaryTree(6))
     right.set_right(BinaryTree(30))
+    
+    # set root and left child
     tree.set_left(left)
+    # set root and right child
     tree.set_right(right)
 
-    print tree.inorder()
+    print 'Preorder: ', tree.preorder()
+    print 'Inorder: ', tree.inorder()
+    print 'Postorder: ', tree.postorder()
--- Ch4/ctcich4.py
+++ Ch4/ctcich4.py
@@ -1,1 +1,31 @@
 # 4.1 Check if a binary tree is balanced(heights of two subtrees of a node do not differ by more than 1)
+import random
+
+class BinaryTree(object):
+	"""docstring for BinaryTree"""
+	def __init__(self, content):
+		self.left = None
+		self.right = None
+		self.content = content
+		self.depth = -1 # depth = -1 when its not been calculated yet
+
+	def __str__(self):
+		return '(' + str(self.left) + ' - ' + str(self.content) + ' - ' + str(self.right) + ')'
+
+
+def depth(btree):
+	if btree is None:
+		return
+	else:
+		if btree.depth != -1:
+			return btree.depth
+		else:
+			btree.depth = 1 + max(depth(btree.left), depth(btree.right))
+			return btree.depth
+
+bt = BinaryTree(random.randint(0, 9))
+for c1 in xrange(0,3):
+    bt2 = BinaryTree(random.randint(0, 9))
+    bt2.left = bt
+    bt = bt2
+print bt.depth
