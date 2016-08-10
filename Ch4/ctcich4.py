27a245f83836db8bb234eeb03d0e8bc9b3bc49d8
Adarsh Nair <adarsh108@gmail.com>
2016-08-10 11:31:44 -0700

    bfs

---
+++ Ch4/ctcich4.py
@@ -0,0 +1,1 @@
+# 4.1 Check if a binary tree is balanced(heights of two subtrees of a node do not differ by more than 1)
--- Ch4/graphs.py
+++ Ch4/graphs.py
@@ -5,6 +5,7 @@ graph = {'A': set(['B', 'C']),
          'E': set(['B', 'F']),
          'F': set(['E'])}
 
+# recursive approach
 def dfs(graph, start, visited = None):
 	if visited == None:
 		visited = set()
@@ -17,6 +18,35 @@ def dfs(graph, start, visited = None):
 		dfs(graph, next, visited)
 		# print '\n'
 	return visited
+# print dfs(graph, 'A')
 
-print dfs(graph, 'A')
+# non recursive
+def dfs_nonrecursive(graph, start):
+	visited = set()
+	stack = [start]
+	while stack:
+		vertex = stack.pop()
+		if vertex not in visited:
+			visited.add(vertex)
+			stack.extend(graph[vertex] - visited)
+	return visited
 
+# print dfs_nonrecursive(graph, 'B')
+
+# BFS non recursive approach: uses queues
+def bfs_nonrecursive(graph, start):
+	visited = set()
+	queue = [start]
+	while queue:
+		# remove item at the front of the queue
+		vertex = queue.pop(0)
+		if vertex not in visited:
+			visited.add(vertex)
+			queue.extend(graph[vertex] - visited)
+	return visited
+
+print bfs_nonrecursive(graph, 'A')
+
+# Paths
+# http://eddmann.com/posts/depth-first-search-and-breadth-first-search-in-python/
+# TODO
