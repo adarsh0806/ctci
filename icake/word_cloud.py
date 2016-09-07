b80854de7450b477f49d879fefd2087e6d4d8e23
Adarsh Nair <adarsh108@gmail.com>
2016-09-07 10:38:01 -0700

    word cloud in one line

--- icake/word_cloud.py
+++ icake/word_cloud.py
@@ -1,11 +1,7 @@
 # icake solution implements Counter and string methods manually. TODO
+from collections import Counter
+import string
 def word_cloud(s):
-	from collections import Counter
-	import string
-	s = s.lower()
-	s = s.translate(string.maketrans('',''), string.punctuation)
-	s = s.split(' ')
-	c = Counter(s)
-	return c
+	return Counter(s.translate(string.maketrans('',''), string.punctuation).lower().split(' '))
 
 print word_cloud('After beating the eggs, Dana read the next step: Add milk and eggs, then add flour and sugar.')
