0f0a2c4f6df4443aa1fa4f81a8e544c2adb2d0d2
Adarsh Nair <adarsh108@gmail.com>
2016-09-08 10:32:57 -0700

    love letter problem

---
+++ icake/love_letter_problem.py
@@ -0,0 +1,18 @@
+# Given a string L representing the letter and a string N representing the newspaper, return true if the L can be written entirely from N and false otherwise.
+# T: O(n), S:(n)
+def pramp(l,n):
+   import string
+   # l = l.translate(string.maketrans('',''), string.punctuation)
+   l = l.replace(' ','')
+   n = n.replace(' ','')
+   from collections import Counter
+   newspaper = Counter(n)
+   love_letter = Counter(l)
+   for key, value in love_letter.iteritems():
+   	if love_letter[key] <= newspaper[key]:
+   		print 'the letter {} in love letter has sufficient chars.', format(key)
+   	else:
+   		print 'the letter {} in love letter does not have sufficient chars.', format(key)
+   		
+   
+print pramp('i love you!', 'i love you so much.')
